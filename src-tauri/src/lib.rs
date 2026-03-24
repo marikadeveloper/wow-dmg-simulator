d055fcd9910512cff736de832296e559004aca0a
Marika Mella <marika.mella@ptsb.ie>
2026-03-24 17:21:59 +0100

    feat: add tauri MCP configuration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,10 +6,17 @@ use commands::run_simc::run_top_gear;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_store::Builder::default().build());
+
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(tauri_plugin_mcp_bridge::init());
+    }
+
+    builder
         .invoke_handler(tauri::generate_handler![
             run_top_gear,
             fetch_item_data,
